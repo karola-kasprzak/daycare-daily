cb1fc1e43a8fc624799865bff56c33fa24b7a877
karola-kasprzak <karola.a.kasprzak@gmail.com>
2021-02-12 21:12:09 +0100

    posts images fixed

--- src/components/Posts/SinglePost.js
+++ src/components/Posts/SinglePost.js
@@ -1,24 +1,41 @@
 import React from "react";
-import { Card, Badge, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import image from "../../images/our_daycare5.jpg";
 
 function SinglePost({ data }) {
     return (
-        <Card className="h-100 shadow-sm bg-white rounded">
+        <Card
+            className="h-100 bg-white "
+            style={{ borderColor: "#abc4b6", height: "500px" }}
+        >
             <Card.Header
                 className="text-secondary small py-0 my0"
                 style={{ backgroundColor: "#abc4b6" }}
             >
-                Date: {data.date}
+                {data.date}
             </Card.Header>
-            <Card.Body className="d-flex flex-column">
-                <div className="d-flex mb-2 justify-content-between">
-                    <Card.Title className="mb-0 font-weight-bold">
-                        {data.title}
-                    </Card.Title>
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <Card.Img
+                        variant="bottom"
+                        src={image}
+                        style={{
+                            height: "100%",
+                            objectFit: "cover",
+                        }}
+                    />
                 </div>
-                <Card.Text className="">{data.desc}</Card.Text>
-            </Card.Body>
-            <Card.Img variant="bottom" src={data.image} />
+                <div class="col-md-8">
+                    <Card.Body className="d-flex flex-column">
+                        <div className="d-flex mb-2 justify-content-between">
+                            <Card.Title className="mb-0 font-weight-bold">
+                                {data.title}
+                            </Card.Title>
+                        </div>
+                        <Card.Text className="">{data.desc}</Card.Text>
+                    </Card.Body>
+                </div>
+            </div>
         </Card>
     );
 }
